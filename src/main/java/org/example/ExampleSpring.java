e2da92069e77de89678b2ad1b7ac1c3f0e5f8481
alex <alex-vav@list.ru>
2022-09-25 23:23:45 +0300

    Lesson 6 Dependency Injection - via setter and  scope prototype- XML

--- src/main/java/org/example/ExampleSpring.java
+++ src/main/java/org/example/ExampleSpring.java
@@ -4,14 +4,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ExampleSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-//        Music music = context.getBean("musicBean", Music.class); // Эта строчка не нужна, так как в тексте программы мы не  будем обращаться к конкретному обьекту , Инициализация пройдет за кулисами.
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml" );
+        MusicPlayer firstPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class); // стало, так как Spring сам запустил инициализацию и в конструкторе подстаил необходимый параметр
-//        player.setMusic(music); - эта строка не нужна, так как  метод setMusic(Music music) запускается  из файла applucationContext.xml - строка  16, за кулисами.
-        player.playMusic();
-        System.out.println(player.getName());
-        System.out.println(player.getVolume());
+        System.out.println(firstPlayer);
+        System.out.println(secondPlayer);
+        firstPlayer.setVolume(10);
+        System.out.println(firstPlayer.getVolume());
+        System.out.println(secondPlayer.getVolume());
+
         context.close();
     }
 }
