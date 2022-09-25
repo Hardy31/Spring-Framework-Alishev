b729fc47d0aa1e4cb6aea343e14de3c6967f9193
alex <alex-vav@list.ru>
2022-09-25 20:12:15 +0300

    Lesson 6 Dependency Injection - via setter - XML

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,15 +1,28 @@
 package org.example;
 
 public class MusicPlayer {
+
+
     private Music music;
 
-    //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
-        System.out.println("implement MusicPlayer");
+//    //IoC
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//        System.out.println("implement MusicPlayer");
+//    }
+
+    public MusicPlayer() {
     }
 
     public void playMusic(){
         System.out.println("Playing:" + music.getSong());
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
     }
 }
