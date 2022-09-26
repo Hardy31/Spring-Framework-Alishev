f3057bc4e4f8244e3a4ef7c5ed3c42b7271bf497
alex <alex-vav@list.ru>
2022-09-26 23:49:44 +0300

    Lesson 10 - Autowired on parametr BED PRACTIC - Annotation

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -6,19 +6,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class MusicPlayer {
 
-
+    @Autowired
     private Music music;
 
-
-    public MusicPlayer() {
-
-        System.out.println("implement MusicPlayer - constructor without parameter");
-    }
-    @Autowired
-    public void setMusic(Music music) {
-        System.out.println("MusicPlayer - DI from setter" + this);
-        this.music = music;
-    }
+//
+//    public MusicPlayer() {
+//        System.out.println("implement MusicPlayer - constructor without parameter");
+//    }
+//
+//    public void setMusic(Music music) {
+//        System.out.println("MusicPlayer - DI from setter" + this);
+//        this.music = music;
+//    }
 
     public void playMusic(){
         System.out.println("MusicPlayer.playMusic:" + music.getSong());
