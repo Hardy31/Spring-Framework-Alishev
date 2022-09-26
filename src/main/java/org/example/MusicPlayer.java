ae5adc7a52bb3c469216a5910b8429ad9e33c239
alex <alex-vav@list.ru>
2022-09-27 00:06:49 +0300

    Lesson 10 - Autowired constructor with two parameters - Annotation

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -6,20 +6,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class MusicPlayer {
 
-    @Autowired
-    private Music music;
 
-//
-//    public MusicPlayer() {
-//        System.out.println("implement MusicPlayer - constructor without parameter");
-//    }
-//
+//    private Music music;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
+
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        System.out.println("implement MusicPlayer - constructor with parameter" + this);
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
+    }
+
 //    public void setMusic(Music music) {
 //        System.out.println("MusicPlayer - DI from setter" + this);
 //        this.music = music;
 //    }
 
     public void playMusic(){
-        System.out.println("MusicPlayer.playMusic:" + music.getSong());
+//        System.out.println("MusicPlayer.playMusic:" + music.getSong());
+        System.out.println("MusicPlayer.playMusic:" + classicalMusic.getSong());
+        System.out.println("MusicPlayer.playMusic:" + rockMusic.getSong());
+
     }
 }
