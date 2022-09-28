d696b22cb236016507c7ae7f7f2d4c7337ac0410
alex <alex-vav@list.ru>
2022-09-28 15:49:29 +0300

    Lesson 13 - translate progect from annotations to java сodу - JavaCode

--- src/main/java/org/example/ExampleSpring.java
+++ src/main/java/org/example/ExampleSpring.java
@@ -1,12 +1,13 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ExampleSpring {
 
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml" );
 
         Computer computer = context.getBean("computer", Computer.class);
         System.out.println(computer.toString());
